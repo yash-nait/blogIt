9c3e16948f0eceacf92da1112952179df0b76b2c
yash-nait <yashnait@gmail.com>
2021-05-18 22:16:11 +0530

    fix api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://blogit4.herokuapp.com/api',
 })
 
 export const insertPage = payload => api.post(`/blog`, payload)
